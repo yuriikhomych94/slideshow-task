cf2ae9beaa06acbe9445fb800bbd194e3bacbb8c
Yurii Khomych <yuriikhomych94@gmail.com>
2021-06-18 16:01:00 +0300

    fix code

--- src/app/slide/slide/slide.component.ts
+++ src/app/slide/slide/slide.component.ts
@@ -1,8 +1,6 @@
-import { CurrencyPipe } from '@angular/common';
-import { CursorError } from '@angular/compiler/src/ml_parser/lexer';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { PresentationItem } from 'src/app/interface/presentation.interface';
-import { SlideService } from 'src/app/service/slide.service';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {PresentationItem} from 'src/app/interface/presentation.interface';
+import {SlideService} from 'src/app/service/slide.service';
 
 @Component({
   selector: 'app-slide',
@@ -14,13 +12,14 @@ export class SlideComponent implements OnInit {
   @ViewChild('videoContainer') videoContainer: ElementRef;
   @ViewChild('imageContainer') imageContainer: ElementRef;
 
-  urlImage: string = "https://test.onsignage.com/PlayerBackend/creative/get/";
+  urlImage = 'https://test.onsignage.com/PlayerBackend/creative/get/';
   playlist: Array<PresentationItem> = [];
-  currentIndex: number = 0;
-  status: boolean = true;
+  currentIndex = 0;
+  status = true;
 
 
-  constructor(private slideSerive: SlideService) { }
+  constructor(private slideSerive: SlideService) {
+  }
 
   ngOnInit(): void {
     this.getData();
@@ -28,35 +27,35 @@ export class SlideComponent implements OnInit {
 
   getData(): void {
     this.slideSerive.getData().subscribe((response) => {
-      for (let i = 0; i < response.playlists.length; i++) {
-        for (let j = 0; j < response.playlists[i].playlistItems.length; j++) {
-          const fileName = this.urlImage + response.playlists[i].playlistItems[j].creativeKey;
-          const duration = response.playlists[i].playlistItems[j].duration;
+      for (const res of response.playlists) {
+        for (const r of res.playlistItems) {
+          const fileName = this.urlImage + r.creativeKey;
+          const durations = r.duration;
           this.playlist.push({
             url: fileName,
-            duration: duration
-          })
+            duration: durations
+          });
         }
       }
       this.startPlay(0);
-    })
+    });
   }
 
-  startPlay(index: number) {
-    let item = this.playlist[index];
-    this.playItem(item)
+  startPlay(index: number): void {
+    const item = this.playlist[index];
+    this.playItem(item);
     setTimeout(() => {
-      if (this.playlist.length == this.currentIndex) {
+      if (this.playlist.length === this.currentIndex) {
         this.currentIndex = 0;
       }
-      this.startPlay(this.currentIndex)
-    }, item.duration * 1000)
+      this.startPlay(this.currentIndex);
+    }, item.duration * 1000);
   }
 
-  playItem(item: PresentationItem) {
+  playItem(item: PresentationItem): void {
     this.currentIndex++;
     const extenstion = item.url.slice(-3);
-    this.status = extenstion == "jpg"
+    this.status = extenstion === 'jpg';
     setTimeout(() => {
       if (this.status) {
         this.imageContainer.nativeElement.src = item.url;
@@ -65,6 +64,6 @@ export class SlideComponent implements OnInit {
         this.videoContainer.nativeElement.muted = false;
         this.videoContainer.nativeElement.play();
       }
-    }, 300)
+    }, 300);
   }
 }
